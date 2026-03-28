f7396c1b47217dcf27c3f905c5fc9b3d27386375
Olen T <elroylilly@gmail.com>
2026-03-28 10:19:45 -0500

    Add file creation and info printing functions to FAT16

--- kernel/fs/fat16.h
+++ kernel/fs/fat16.h
@@ -6,6 +6,13 @@
 #include <stdint.h>
 #include "../partition/partition.h"
 #include "../drivers/drives.h"
+#include "fs.h"
+
 struct drive_fs_t *fat16_drive_open( struct kdrive_t *drive, struct partition_t *partition );
+
+int fat16_create_file( struct drive_fs_t *fs, char *name,
+                       const uint8_t *content, size_t len );
+
+void fat16_print_info( struct drive_fs_t *fs, uint8_t color );
 
 #endif // FAT16_H
